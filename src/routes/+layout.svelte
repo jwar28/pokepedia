bb95cea32eab74267249c42ca95f77f70cbebd59
jwar28 <javierguerra2203@gmail.com>
2023-05-03 14:03:07 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { AppShell } from '@skeletonlabs/skeleton';
+
+	// Your custom Skeleton theme:
+	import '../theme.postcss';
+
+	// This contains the bulk of Skeletons required styles:
+	import '@skeletonlabs/skeleton/styles/all.css';
+
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	import '../app.postcss';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">Header</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="footer">
+		<div class="flex justify-center mb-1">
+			<button>
+				<a href="https://github.com/jwar28" target="_blank"
+					><img
+						src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+						class="w-6 h-auto"
+						alt="github-logo"
+					/></a
+				>
+			</button>
+		</div></svelte:fragment
+	>
+</AppShell>
