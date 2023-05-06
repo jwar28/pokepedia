dfb6fc3fe57f467faae98486a06f5ade7b804841
jwar28 <javierguerra2203@gmail.com>
2023-05-06 14:41:01 -0500

    mobile nav correction

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -2,7 +2,9 @@
 </script>
 
 <nav class="list-nav">
-	<ul class="lg:flex lg:justify-evenly lg:items-center lg:text-white">
+	<ul
+		class="lg:flex lg:justify-evenly lg:items-center lg:text-white max-sm:p-5 max-sm:flex max-sm:flex-col max-sm:gap-5"
+	>
 		<li><a href="/">Inicio</a></li>
 		<li><a href="/">Items</a></li>
 		<li><a href="/">Randomizer</a></li>
