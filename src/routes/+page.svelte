2b9ea1b4bc18332e57ee85ae1c1a15b839271b3b
jwar28 <javierguerra2203@gmail.com>
2023-05-06 15:39:16 -0500

    style corrections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -88,7 +88,7 @@
 </script>
 
 <!-- Header -->
-<div class="sticky top-0 bg-[#e4e6ee] w-full z-30 py-2">
+<div class="sticky top-0 bg-[#e4e6ee] w-full z-30 py-2 shadow-md">
 	<div class="flex justify-center">
 		<div class="flex justify-center w-full max-lg:flex-wrap max-lg:gap-2 gap-10">
 			<p class="flex items-center">Filtrar por:</p>
