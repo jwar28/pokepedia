4377918054a493933a7d287df11b82c2ac046544
jwar28 <javierguerra2203@gmail.com>
2023-05-06 13:36:28 -0500

    pokemon information layout fixed

--- src/lib/components/PokemonStatCard.svelte
+++ src/lib/components/PokemonStatCard.svelte
@@ -18,7 +18,7 @@
 	});
 </script>
 
-<!-- popup -->
+<!-- popupya -->
 <div class="card variant-filled-secondary p-4" data-popup="goBackPopup">
 	Volver
 	<div class="arrow variant-filled-secondary" />
@@ -29,28 +29,32 @@
 	<div>
 		<!-- Image -->
 		<div class="bg-[url('/background.svg')] bg-cover bg-blue-300 card h-full shadow-md">
-			<!-- Go back button -->
-			<button
-				type="button"
-				class="absolute btn"
-				use:popup={popupSettings}
-				on:click={() => history.back()}
-			>
-				<svg
-					aria-hidden="true"
-					class="w-8 h-8"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M2.407 13.058a1.495 1.495 0 0 1 0-2.116L8.04 5.3c.39-.39 1.03-.39 1.42 0 .39.39.39 1.02 0 1.41L6.744 9.427a9.992 9.992 0 0 1-1.768 1.406l-.342.214.208.207A10 10 0 0 1 7.082 11h13.92a1 1 0 1 1 0 2H7.083a10 10 0 0 1-2.24-.254l-.208.207.342.214c.641.4 1.233.872 1.768 1.406L9.46 17.29c.39.39.39 1.02 0 1.41-.39.39-1.03.39-1.42 0l-5.633-5.642z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-			</button>
-			<div class="flex justify-center items-center h-full">
-				<img src={pokemon.sprites.front_default} alt="pokemon" class="w-72 group-hover:hidden" />
+			<div class="flex flex-col h-full">
+				<div>
+					<!-- Go back button -->
+					<button
+						type="button"
+						class="btn"
+						use:popup={popupSettings}
+						on:click={() => history.back()}
+					>
+						<svg
+							aria-hidden="true"
+							class="w-8 h-8"
+							fill="currentColor"
+							viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg"
+							><path
+								fill-rule="evenodd"
+								d="M2.407 13.058a1.495 1.495 0 0 1 0-2.116L8.04 5.3c.39-.39 1.03-.39 1.42 0 .39.39.39 1.02 0 1.41L6.744 9.427a9.992 9.992 0 0 1-1.768 1.406l-.342.214.208.207A10 10 0 0 1 7.082 11h13.92a1 1 0 1 1 0 2H7.083a10 10 0 0 1-2.24-.254l-.208.207.342.214c.641.4 1.233.872 1.768 1.406L9.46 17.29c.39.39.39 1.02 0 1.41-.39.39-1.03.39-1.42 0l-5.633-5.642z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</button>
+				</div>
+				<div class="flex justify-center items-center h-full">
+					<img src={pokemon.sprites.front_default} alt="pokemon" class="w-72 group-hover:hidden" />
+				</div>
 			</div>
 		</div>
 	</div>
