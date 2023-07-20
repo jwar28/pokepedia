18f9bd75d4fa0e13453d6838217c86ed77355873
jwar28 <javierguerra2203@gmail.com>
2023-07-19 21:05:22 -0500

    feat(404): 404 page translated to english

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,8 @@
 <div>
 	<a href="/">
-		<div class="flex gap-3">
-			<img src="/pokepedia-logo.png" alt="logo" class="w-64 h-24" />
-			<img src="/pokeball.png" alt="pokeball" class="w-20 h-24 max-md:hidden" />
+		<div class="flex">
+			<img src="/pokepedia-logo.png" alt="logo" class="w-fit h-20" />
+			<img src="/pokeball-gif.gif" alt="pokeball" class="w-fit h-20 max-md:hidden" />
 		</div>
 	</a>
 </div>
